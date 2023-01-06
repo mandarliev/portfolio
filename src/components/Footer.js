2322bd60ecce2df915cfe208c646053e6ea4b6c7
Dimitar <dimitar.mandarliev@gmail.com>
2023-01-06 21:48:05 +0200

    Added the links to the icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import "../styles/Footer.css"
+import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
-        <div className='socialMedia'>
-            <InstagramIcon/>
-            <TwitterIcon/>
-            <FacebookIcon/>
-            <LinkedInIcon/>
-        </div>
-        <p> &copy; 2023 mandarliev.com</p>
+    <div className="footer">
+      <div className="socialMedia">
+        <a href="https://www.instagram.com/d_mandarliev/">
+          <InstagramIcon />
+        </a>
+        <a href="https://twitter.com/mandarliev">
+          <TwitterIcon />
+        </a>
+        <a href="https://www.facebook.com/dimitar.mandarliev.3/">
+          <FacebookIcon />
+        </a>
+        <a href="https://www.linkedin.com/in/dimitar-mandarliev-515273172/">
+          <LinkedInIcon />
+        </a>
+      </div>
+      <p> &copy; 2023 mandarliev.com</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,49 @@
-import React from 'react'
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import "../styles/Home.css"
+import React from "react";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='about'>
+    <div className="home">
+      <div className="about">
         <h2>Hi, My Name is Dimitar</h2>
-        <div className='prompt'>
-          <p>A Technical Product Manager with a passion to become a web developer.</p>
-          <LinkedInIcon/>
-          <EmailIcon/>
-          <GitHubIcon/>
+        <div className="prompt">
+          <p>
+            A Technical Product Manager with a passion to become a web
+            developer.
+          </p>
+          <a href="https://www.linkedin.com/in/dimitar-mandarliev-515273172/">
+            <LinkedInIcon />
+          </a>
+          <a href="mailto: dimitar.mandarliev@gmail.com">
+            <EmailIcon />
+          </a>
+          <a href="https://github.com/mandarliev">
+            <GitHubIcon />
+          </a>
         </div>
       </div>
-      <div className='skills'>
+      <div className="skills">
         <h1>Skills</h1>
-        <ol className='list'>
-          <li className='item'>
+        <ol className="list">
+          <li className="item">
             <h2>Front-End</h2>
             <span>ReactJS, Redux, HTML, CSS, NPM, MaterialUI, Bootstrap</span>
           </li>
-          <li className='item'>
+          <li className="item">
             <h2>Back-End</h2>
             <span>MySQL, MS SQL</span>
           </li>
-          <li className='item'>
+          <li className="item">
             <h2>Languages</h2>
             <span>JavaScript, Java, Kotlin</span>
           </li>
         </ol>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
